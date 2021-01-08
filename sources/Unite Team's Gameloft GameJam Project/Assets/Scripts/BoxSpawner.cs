feedd544a8ed8cb914e9fec7675004e3ef410c7e
phansonloc1999 <phansonloc1999@gmail.com>
2021-01-08 22:33:59 +0700

    Add box's swinging back and forth

--- sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxSpawner.cs
+++ sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxSpawner.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using DG.Tweening;
 using UnityEngine;
 
 public class BoxSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _boxPrefab;
 
+    [SerializeField] private float _eachHorizontalSwingDuration;
+
+    private Vector3 _cameraLeftEdgeWorldPos;
+    private Vector3 _cameraRightEdgeWorldPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        SpawnBox();
+        Init();
+
+        var newBox = SpawnBox();
+        BackForthHorizontalSwing(newBox);
     }
 
     // Update is called once per frame
@@ -18,13 +25,25 @@ public class BoxSpawner : MonoBehaviour
 
     }
 
-    public void SpawnBox()
+    private void Init()
     {
-        Instantiate(_boxPrefab, transform.position, Quaternion.identity);
+        _cameraLeftEdgeWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
+        _cameraRightEdgeWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+
+        // Start x position is at camera's right edge world pos x
+        transform.position = new Vector3(_cameraRightEdgeWorldPos.x, transform.position.y, 0);
     }
 
-    public void MoveHorizontally()
+    public GameObject SpawnBox()
     {
+        return Instantiate(_boxPrefab, transform.position, Quaternion.identity);
+    }
 
+    public void BackForthHorizontalSwing(GameObject newBox)
+    {
+        var swingSeq = DOTween.Sequence()
+        .Append(newBox.transform.DOMoveX(_cameraLeftEdgeWorldPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
+        .Append(newBox.transform.DOMoveX(_cameraRightEdgeWorldPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
+        .SetLoops(-1);
     }
 }
