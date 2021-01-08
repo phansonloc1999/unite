ec5bbf7ef2e89bb7f71176eb6b069ef654a61e52
phansonloc1999 <phansonloc1999@gmail.com>
2021-01-08 21:52:08 +0700

    Add DOTween, BoxSpawner

---
+++ sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _boxPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnBox();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SpawnBox()
+    {
+        Instantiate(_boxPrefab, transform.position, Quaternion.identity);
+    }
+
+    public void MoveHorizontally()
+    {
+
+    }
+}
