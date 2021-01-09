7e16878d44173ab4c54f893f93361bb6f0b78a88
phansonloc1999 <phansonloc1999@gmail.com>
2021-01-09 16:34:16 +0700

    Add box circular movement

---
+++ sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxCircularMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCircularMovement : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed = 5f;
+    [SerializeField] private float _radius = 0.1f;
+
+    private Vector2 _centre;
+    private float _angle;
+
+    private void Start()
+    {
+        _centre = transform.position;
+    }
+
+    private void Update()
+    {
+        _angle += _rotateSpeed * Time.deltaTime;
+
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * _radius;
+        transform.position = _centre + offset;
+    }
+}
