fd958b0a2c6549099f63b361881be0dee637eed6
phansonloc1999 <phansonloc1999@gmail.com>
2021-01-09 08:38:17 +0700

    Add spawning new box 2 second after dropping

--- sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxSpawner.cs
+++ sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxSpawner.cs
@@ -1,4 +1,5 @@
-﻿using DG.Tweening;
+﻿using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class BoxSpawner : MonoBehaviour
@@ -7,7 +8,14 @@ public class BoxSpawner : MonoBehaviour
 
     [SerializeField] private float _eachHorizontalSwingDuration;
 
+    [SerializeField] private GameObject _newBox = null;
+
+    [SerializeField] private float _dropAndSpawnBoxInterval;
+
+    private Sequence _newBoxMoveSequence;
+
     private Vector3 _cameraLeftEdgeWorldPos;
+
     private Vector3 _cameraRightEdgeWorldPos;
 
     // Start is called before the first frame update
@@ -15,14 +23,28 @@ public class BoxSpawner : MonoBehaviour
     {
         Init();
 
-        var newBox = SpawnBox();
-        BackForthHorizontalSwing(newBox);
+        SpawnNewBox();
+        BackForthHorizontalSwing();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (_newBox != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                IEnumerator MyCouroutine()
+                {
+                    DropNewBox();
+                    yield return new WaitForSeconds(_dropAndSpawnBoxInterval);
+                    SpawnNewBox();
+                    BackForthHorizontalSwing();
+                    yield break;
+                }
+                StartCoroutine(MyCouroutine());
+            }
+        }
     }
 
     private void Init()
@@ -34,16 +56,23 @@ public class BoxSpawner : MonoBehaviour
         transform.position = new Vector3(_cameraRightEdgeWorldPos.x, transform.position.y, 0);
     }
 
-    public GameObject SpawnBox()
+    private void DropNewBox()
     {
-        return Instantiate(_boxPrefab, transform.position, Quaternion.identity);
+        _newBoxMoveSequence.Kill();
+        // Reapply gravity scale to new box
+        _newBox.GetComponent<Rigidbody2D>().gravityScale = 1;
     }
 
-    public void BackForthHorizontalSwing(GameObject newBox)
+    public void SpawnNewBox()
     {
-        var swingSeq = DOTween.Sequence()
-        .Append(newBox.transform.DOMoveX(_cameraLeftEdgeWorldPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
-        .Append(newBox.transform.DOMoveX(_cameraRightEdgeWorldPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
+        _newBox = Instantiate(_boxPrefab, transform.position, Quaternion.identity);
+    }
+
+    public void BackForthHorizontalSwing()
+    {
+        _newBoxMoveSequence = DOTween.Sequence()
+        .Append(_newBox.transform.DOMoveX(_cameraLeftEdgeWorldPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
+        .Append(_newBox.transform.DOMoveX(_cameraRightEdgeWorldPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
         .SetLoops(-1);
     }
 }
