4a4a5765cd1d9977fa9216781c6601d99037e002
phansonloc1999 <phansonloc1999@gmail.com>
2021-01-09 09:51:06 +0700

    Update caamera & spawner pos after dropping new box

--- sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxSpawner.cs
+++ sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxSpawner.cs
@@ -12,11 +12,15 @@ public class BoxSpawner : MonoBehaviour
 
     [SerializeField] private float _dropAndSpawnBoxInterval;
 
+    [SerializeField] private Camera _mainCamera;
+
     private Sequence _newBoxMoveSequence;
 
-    private Vector3 _cameraLeftEdgeWorldPos;
+    private Vector3 _camLeftWorldPos;
 
-    private Vector3 _cameraRightEdgeWorldPos;
+    private Vector3 _camTopRightPos;
+
+    private static float _camHeight;
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +42,8 @@ public class BoxSpawner : MonoBehaviour
                 {
                     DropNewBox();
                     yield return new WaitForSeconds(_dropAndSpawnBoxInterval);
+                    UpdateCameraSpawnerPos();
+                    GetCamLeftRightPos();
                     SpawnNewBox();
                     BackForthHorizontalSwing();
                     yield break;
@@ -49,11 +55,12 @@ public class BoxSpawner : MonoBehaviour
 
     private void Init()
     {
-        _cameraLeftEdgeWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
-        _cameraRightEdgeWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        GetCamLeftRightPos();
 
-        // Start x position is at camera's right edge world pos x
-        transform.position = new Vector3(_cameraRightEdgeWorldPos.x, transform.position.y, 0);
+        // Start position is at camera's top right edge world pos
+        transform.position = new Vector3(_camTopRightPos.x, _camTopRightPos.y, 0);
+
+        _camHeight = _mainCamera.orthographicSize * 2.0f;
     }
 
     private void DropNewBox()
@@ -63,16 +70,29 @@ public class BoxSpawner : MonoBehaviour
         _newBox.GetComponent<Rigidbody2D>().gravityScale = 1;
     }
 
-    public void SpawnNewBox()
+    private void SpawnNewBox()
     {
         _newBox = Instantiate(_boxPrefab, transform.position, Quaternion.identity);
     }
 
-    public void BackForthHorizontalSwing()
+    private void BackForthHorizontalSwing()
     {
         _newBoxMoveSequence = DOTween.Sequence()
-        .Append(_newBox.transform.DOMoveX(_cameraLeftEdgeWorldPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
-        .Append(_newBox.transform.DOMoveX(_cameraRightEdgeWorldPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
+        .Append(_newBox.transform.DOMoveX(_camLeftWorldPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
+        .Append(_newBox.transform.DOMoveX(_camTopRightPos.x, _eachHorizontalSwingDuration).SetEase(Ease.Linear))
         .SetLoops(-1);
+    }
+
+    private void GetCamLeftRightPos()
+    {
+        _camLeftWorldPos = _mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0));
+        _camTopRightPos = _mainCamera.ScreenToWorldPoint(new Vector3(0, Screen.height, 0));
+    }
+
+    private void UpdateCameraSpawnerPos()
+    {
+        _mainCamera.transform.position = new Vector3(_mainCamera.transform.position.x, _newBox.transform.position.y + _camHeight / 8.0f, _mainCamera.transform.position.z);
+        var camTopCenterPos = _mainCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height, 0));
+        transform.position = new Vector3(camTopCenterPos.x, camTopCenterPos.y, transform.position.z);
     }
 }
