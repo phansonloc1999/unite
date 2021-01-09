e2d4aa0c59eb9189db140901f936edbab3e3fec0
phansonloc1999 <phansonloc1999@gmail.com>
2021-01-09 18:49:23 +0700

    Add A & P box graphics

--- sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxCollision.cs
+++ sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/BoxCollision.cs
@@ -3,15 +3,7 @@ using UnityEngine;
 
 public class BoxCollision : MonoBehaviour
 {
-    [SerializeField] private float _collisionStayDuration;
-
-    private void OnCollisionStay2D(Collision2D other)
+    private void Update()
     {
-        IEnumerator MyCouroutine()
-        {
-            yield return new WaitForSeconds(5.0f);
-            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
-        }
-        StartCoroutine(MyCouroutine());
     }
 }
