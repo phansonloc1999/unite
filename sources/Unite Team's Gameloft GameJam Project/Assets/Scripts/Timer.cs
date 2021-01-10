458093a89799388b13b39580758473ee191927a9
phansonloc1999 <phansonloc1999@gmail.com>
2021-01-10 09:25:28 +0700

    Add remaining timer text

--- sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/Timer.cs
+++ sources/Unite Team's Gameloft GameJam Project/Assets/Scripts/Timer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] private float _remainingTime;
 
     [SerializeField] private float _maxPlayTime;
+
+    [SerializeField] private Text _text;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,7 @@ public class Timer : MonoBehaviour
     void Update()
     {
         _remainingTime -= Time.deltaTime;
+        _text.text = "Time left: " + (int)_remainingTime;
 
         if (_remainingTime <= 0)
         {
